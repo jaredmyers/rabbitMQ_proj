c15c6a123e9532aa4c049dd25041301953d48845
Jared Myers <jaredjmyers@gmail.com>
2021-10-28 15:28:24 -0400

    renamed, organizing

---
+++ django_box/spotify/site_spotify/credentials.py
@@ -0,0 +1,10 @@
+user = 'taco'
+pw = 'tastegood'
+ip_address = 'localhost'
+
+client_id = '4ed36c33643a4974817619f60d7615cd'
+client_sk = '7db6c1bc9d2f4a328f0968700b95fc69'
+#redirect_uri = 'http://localhost:8080/'
+redirect_uri = 'http://localhost:8080/'
+#spot_user = 'rebeccasork'
+spot_user = 'jaredrunner'
