4141fd47425e97f6e0d9caa148b1cec02df17b24
Wyatt Eynon <wyatteynon@gmail.com>
2021-11-09 23:10:02 -0500

    added fleshed out compare functions that are integrated to the database

--- db_box/db_accessor_methods.py
+++ db_box/db_accessor_methods.py
@@ -598,163 +598,217 @@ def accessor_methods(body,queue):
         return [current_user, query_result]
 
 
-    def getUsersForComparison(body):
+    def getUsersForListComparison(body):
+        body = body.split('?&#))')
+        sessionId = body[1]
+        #typeOfRequest= body[2]
+        #Type of request is either "get_list_info" or "get_detailed info"
+        # List info is used when displaying the list of users. Get detailed info is when getting a specific user
 
-        users="Query return TUPLE OF JSON object from users"
-        usernames="Query return TUPLE of Usernames for each JSON object"
+        tableData=[]
+        tableData=get_compare_data(sessionId)
+        currentUsername=tableData[0]
+        currentJSON=""
+        comparisonListDetails=[]
+        sortDict={}
+
+        if len(tableData) <= 1:
+            raise Exception("Not enough user information in database to make comparison list!")
+
+        for userObject in tableData[1]: #in the spirit of as little queries as possible hahahaha
+            if userObject[0]==currentUsername:
+                currentJSON=userObject[1]
         
-        for user in users:
-            #i dont know?!??!
-            pass
+        for userObject in tableData[1]:
+            if userObject[0]==currentUsername:
+                pass
+            else:
+                listingDetails=compare_users(currentJSON,userObject[1],IS_SIMPLE=True)
+                comparisonListDetails.append([userObject[0],listingDetails])
+                sortDict[userObject[0]]=listingDetails[1]
+        sortedSortDict=sorted(sortDict.items(), key=lambda x: x[1], reverse=True)
+        
+        sortedReturnList=[]
 
-        def compare_users(userJSON1,userJSON2,is_simple=False):
+        for entry in sortedSortDict:
+            for compObj in comparisonListDetails:
+                if compObj[0]==entry:
+                    sortedReturnList.append(compObj)
+        
+        return(sortedReturnList)
 
-            def compareGenres(JSONdata1,JSONdata2):
-                #print(JSONdata1["genres"])
-                #graspingForStraws=False
-                genreMatches=[]
-                for genre in JSONdata1["genres"]:
-                    #print(genre)
-                    if genre in JSONdata2["genres"]:
-                        genreMatches.append(genre)
+    def compareUsersDetailed(body):
+        sessionId=body[1]
+        specifiedUserUsername=body[2]
+        
+        try:
+            bigCompareData=get_compare_data(sessionId)
+            currentUserUsername=bigCompareData[0]
+        except:
+            raise Exception("Could not cycle through database")
 
-                if(len(genreMatches))<7:
-                    if "rock" not in genreMatches:
-                        genreMatches.append("rock")
-                    return(genreMatches)
-                else:
-                    for genre in genreMatches:
-                        badGenres=["alternative rock","rock","pop","electronic"]
-                        if len(genreMatches)>=7:
-                            for badGenre in badGenres:
-                                if badGenre==genre:
-                                    genreMatches.remove(genre)
-                #print(genreMatches)
+        currentUserJSON=""
+        specifiedUserJSON=""
+
+        for userDataObj in bigCompareData[1]:
+            if userDataObj[0]==specifiedUserUsername:
+                specifiedUserJSON=userDataObj[1]
+            if userDataObj[0]==currentUserUsername:
+                currentUserJSON=userDataObj[1]
+        try:
+            return(compare_users(currentUserJSON,specifiedUserJSON,IS_SIMPLE=False))
+        except:
+            raise Exception("Could not Compare User JSON Files in Detailed Format") 
+            
+
+    def compare_users(userJSON1,userJSON2,IS_SIMPLE=False):
+
+        def compareGenres(JSONdata1,JSONdata2):
+            #print(JSONdata1["genres"])
+            #graspingForStraws=False
+            genreMatches=[]
+            for genre in JSONdata1["genres"]:
+                #print(genre)
+                if genre in JSONdata2["genres"]:
+                    genreMatches.append(genre)
+
+            if(len(genreMatches))<7:
+                if "rock" not in genreMatches:
+                    genreMatches.append("rock")
                 return(genreMatches)
+            else:
+                for genre in genreMatches:
+                    badGenres=["alternative rock","rock","pop","electronic"]
+                    if len(genreMatches)>=7:
+                        for badGenre in badGenres:
+                            if badGenre==genre:
+                                genreMatches.remove(genre)
+            #print(genreMatches)
+            return(genreMatches)
 
-            def compareTracks(JSONdata1,JSONdata2,isTopTracks=True):
-                #Returns 3 Lists in a larger list object
-                #Index 0 = a list of track objects (dictionaries) of tracks that were a 1 to 1 match between top 50 songs
-                #Index 1 = a list of 
-                #print((JSONdata1["tracks"][0])['artist'])
-                tracks1=JSONdata1["tracks"]
-                tracks2=JSONdata2["tracks"]
-                if isTopTracks == False:
-                    tracks1=JSONdata1["libraryTracks"]
-                    tracks2=JSONdata2["libraryTracks"]
+        def compareTracks(JSONdata1,JSONdata2,isTopTracks=True):
+            #Returns 3 Lists in a larger list object
+            #Index 0 = a list of track objects (dictionaries) of tracks that were a 1 to 1 match between top 50 songs
+            #Index 1 = a list of 
+            #print((JSONdata1["tracks"][0])['artist'])
+            tracks1=JSONdata1["tracks"]
+            tracks2=JSONdata2["tracks"]
+            if isTopTracks == False:
+                tracks1=JSONdata1["libraryTracks"]
+                tracks2=JSONdata2["libraryTracks"]
 
-                trackMatches1to1=[]
-                trackArtistMatchesChecklist=[]
-                trackArtistMatchesReturnList=[]
-                trackAlbumMatchesChecklist=[]
-                trackAlbumMatchesReturnList=[]
-                #print(tracks1[0])
+            trackMatches1to1=[]
+            trackArtistMatchesChecklist=[]
+            trackArtistMatchesReturnList=[]
+            trackAlbumMatchesChecklist=[]
+            trackAlbumMatchesReturnList=[]
+            #print(tracks1[0])
 
-                def findInReturnList(sampReturnList,checkObj):
-                    if(len(sampReturnList)!=0):
-                        for itemObj in sampReturnList:
-                            if itemObj[0]==checkObj:
-                                itemObj[1]=itemObj[1]+1
+            def findInReturnList(sampReturnList,checkObj):
+                if(len(sampReturnList)!=0):
+                    for itemObj in sampReturnList:
+                        if itemObj[0]==checkObj:
+                            itemObj[1]=itemObj[1]+1
 
-                for track1 in tracks1:
-                    track1Name=track1["name"]
-                    for track2 in tracks2:
-                        #print( track1["artist"]+" versus "+track2["artist"])
-                        print(track1.keys())
-                        if ((track1Name==track2["name"])):
-                            #print("Track MATCH!"+track1Name)
-                            trackMatches1to1.append(track1)
+            for track1 in tracks1:
+                track1Name=track1["name"]
+                for track2 in tracks2:
+                    #print( track1["artist"]+" versus "+track2["artist"])
+                    print(track1.keys())
+                    if ((track1Name==track2["name"])):
+                        #print("Track MATCH!"+track1Name)
+                        trackMatches1to1.append(track1)
+                    #print(track1['album_name'])
+                    if track1["album_name"]==track2["album_name"]:
                         #print(track1['album_name'])
-                        if track1["album_name"]==track2["album_name"]:
-                            #print(track1['album_name'])
-                            if track1["album_name"] not in trackAlbumMatchesChecklist:
-                                trackAlbumMatchesChecklist.append(track1["album_name"])
-                                trackArtistMatchesReturnList.append([track1["album_name"],1])
-                            if track1["album_name"] in trackAlbumMatchesChecklist:
-                                findInReturnList(trackAlbumMatchesReturnList,track1["album_name"])
-                        if (track1["artist"]==track2["artist"]):
-                            #print("WE GOT A BINGO "+track1["artist"])
-                            if track1["artist"] not in trackArtistMatchesChecklist:
-                                trackArtistMatchesChecklist.append(track1["artist"])
-                                trackArtistMatchesReturnList.append([track1["artist"],1])
-                            else:
-                                findInReturnList(trackArtistMatchesReturnList,track2["artist"])
+                        if track1["album_name"] not in trackAlbumMatchesChecklist:
+                            trackAlbumMatchesChecklist.append(track1["album_name"])
+                            trackArtistMatchesReturnList.append([track1["album_name"],1])
+                        if track1["album_name"] in trackAlbumMatchesChecklist:
+                            findInReturnList(trackAlbumMatchesReturnList,track1["album_name"])
+                    if (track1["artist"]==track2["artist"]):
+                        #print("WE GOT A BINGO "+track1["artist"])
+                        if track1["artist"] not in trackArtistMatchesChecklist:
+                            trackArtistMatchesChecklist.append(track1["artist"])
+                            trackArtistMatchesReturnList.append([track1["artist"],1])
+                        else:
+                            findInReturnList(trackArtistMatchesReturnList,track2["artist"])
 
 
-                returnList=[trackMatches1to1,trackArtistMatchesReturnList,trackAlbumMatchesReturnList]
-                #print(trackAlbumMatchesReturnList)
-                #print("\n")
-                return(returnList)
+            returnList=[trackMatches1to1,trackArtistMatchesReturnList,trackAlbumMatchesReturnList]
+            #print(trackAlbumMatchesReturnList)
+            #print("\n")
+            return(returnList)
 
-            def compareArtistsFollowed(JSONdata1,JSONdata2):
-                #print(JSONdata1.keys())
-                sameArtists=[]
-                #print(JSONdata1['followedArtists'])
-                for artist1 in JSONdata1["followedArtists"]:
-                    for artist2 in JSONdata2["followedArtists"]:
-                        if artist1[0] == artist2[0]:
-                            sameArtists.append(artist1)
-                #print(sameArtists)
-                return sameArtists
+        def compareArtistsFollowed(JSONdata1,JSONdata2):
+            #print(JSONdata1.keys())
+            sameArtists=[]
+            #print(JSONdata1['followedArtists'])
+            for artist1 in JSONdata1["followedArtists"]:
+                for artist2 in JSONdata2["followedArtists"]:
+                    if artist1[0] == artist2[0]:
+                        sameArtists.append(artist1)
+            #print(sameArtists)
+            return sameArtists
 
-            def compareFreqListenedToArtists(JSONdata1,JSONdata2):
-                sameFreqArtists=[]
-                #print((JSONdata1["artistFreqByTopTracks"]).keys())
-                for artist1 in JSONdata1["artistFreqByTopTracks"]:
-                    for artist2 in JSONdata2["artistFreqByTopTracks"]:
-                        if artist1 == artist2:
-                            #print(artist2)
-                            pass
+        def compareFreqListenedToArtists(JSONdata1,JSONdata2):
+            sameFreqArtists=[]
+            #print((JSONdata1["artistFreqByTopTracks"]).keys())
+            for artist1 in JSONdata1["artistFreqByTopTracks"]:
+                for artist2 in JSONdata2["artistFreqByTopTracks"]:
+                    if artist1 == artist2:
+                        #print(artist2)
+                        pass
 
-            def compareAlbums(JSONdata1,JSONdata2):
-                #print(JSONdata1['savedAlbums'])
-                albumsMatched=[]
-                for album1 in JSONdata1["savedAlbums"]:
-                    for album2 in JSONdata2["savedAlbums"]:
-                        if album1[1] == album2[1]:
-                            albumsMatched.append(album1)
-                #print((albumsMatched))
-                return(albumsMatched)  
+        def compareAlbums(JSONdata1,JSONdata2):
+            #print(JSONdata1['savedAlbums'])
+            albumsMatched=[]
+            for album1 in JSONdata1["savedAlbums"]:
+                for album2 in JSONdata2["savedAlbums"]:
+                    if album1[1] == album2[1]:
+                        albumsMatched.append(album1)
+            #print((albumsMatched))
+            return(albumsMatched)  
 
+        try:
+            #Pull from user 1
+            f = open('sampleDBpull4.json')
+            dataA = json.load(f)
+            f.close()
+
+            #Pull from user 2
+            f = open('sampleDBpull2.json')
+            dataB = json.load(f)
+            f.close()
+        except:
+            print("File error")
+
+        output=[]
+        try:
+            #print(dataB.keys())
+            output.append(compareTracks(dataA,dataB))
+            output.append(compareTracks(dataA,dataB,isTopTracks=False))
+            output.append(compareGenres(dataA,dataB))
+            output.append(compareArtistsFollowed(dataA,dataB))
+            output.append(compareAlbums(dataA,dataB))
+            #print(compareAlbums(dataA,dataB))
+            #output.append()
+            #compareFreqListenedToArtists(dataA,dataB)
+            #print(dataA.keys())
+            #print(output)
+            #with open('CompareOutputSample.json', 'w') as f:
+                #json.dump(output, f)
             try:
-                #Pull from user 1
-                f = open('sampleDBpull4.json')
-                dataA = json.load(f)
-                f.close()
-
-                #Pull from user 2
-                f = open('sampleDBpull2.json')
-                dataB = json.load(f)
-                f.close()
+                if IS_SIMPLE==False:
+                    return(output)
+                else:
+                    return([dataB["username"],len(output)])
             except:
-                print("File error")
-
-            output=[]
-            try:
-                #print(dataB.keys())
-                output.append(compareTracks(dataA,dataB))
-                output.append(compareTracks(dataA,dataB,isTopTracks=False))
-                output.append(compareGenres(dataA,dataB))
-                output.append(compareArtistsFollowed(dataA,dataB))
-                output.append(compareAlbums(dataA,dataB))
-                #print(compareAlbums(dataA,dataB))
-                #output.append()
-                #compareFreqListenedToArtists(dataA,dataB)
-                #print(dataA.keys())
-                #print(output)
-                with open('CompareOutputSample.json', 'w') as f:
-                    json.dump(output, f)
-                try:
-                    if IS_SIMPLE==False:
-                        return(output)
-                    else:
-                        return([dataB["username"],len(output)])
-                except:
-                    print("Error returning program output. Make sure you are calling the function with the correct IS_SIMPLE value")
-            except KeyError:
-                print("The JSON files you're using produced a key error. \n This can happen when using an old version of the 'Long JSON' output. Recompute the User info with the userInfoPuller script and ")
-            except:
-                print("Something went wrong in the comparison functions.")
+                print("Error returning program output. Make sure you are calling the function with the correct IS_SIMPLE value")
+        except KeyError:
+            print("The JSON files you're using produced a key error. \n This can happen when using an old version of the 'Long JSON' output. Recompute the User info with the userInfoPuller script and ")
+        except:
+            print("Something went wrong in the comparison functions.")
 
 
 ## Main entry point
